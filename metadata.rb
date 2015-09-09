96a2a775efab31495cb6f4056be65348d68b3b22
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-09-09 16:46:50 +0300

    add fail2ban deps

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'lauri.jesmin@cashongo.co.uk'
 license          'All rights reserved'
 description      'Installs/Configures cog_ips'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends         'yum-epel','~> 0.6.2'
+depends         'fail2ban','~> 2.3.0'
