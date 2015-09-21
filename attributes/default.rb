5e6fda8f3a42d214b836c06fa761004ac2e3a7f6
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-09-21 16:02:33 +0300

    attributes for ssh keys and version

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,3 +7,7 @@ default['fail2ban']['services'] = {
     "maxretry" => "6"
   }
 }
+default['cog_ips']['suricata_version'] = '2.0.8'
+default['cog_ips']['rules_deploy_vault'] = 'cog_ips'
+default['cog_ips']['rules_deploy_bucket'] = 'deploykeys'
+default['cog_ips']['rules_repo'] = 'git@github.com:cog-peachy/chef-peachy_ips-rules.git'
