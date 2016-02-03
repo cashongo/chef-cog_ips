8d3e4d47af1e82f9fec2aea6e95c61da4b582d7e
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2016-02-03 12:12:55 +0200

    update suricata to 3.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default['fail2ban']['services'] = {
     "maxretry" => "6"
   }
 }
-default['cog_ips']['suricata_version'] = '2.0.10'
+default['cog_ips']['suricata_version'] = '3.0'
 default['cog_ips']['rules_deploy_vault'] = 'cog_ips'
 default['cog_ips']['rules_deploy_bucket'] = 'deploykeys'
 default['cog_ips']['rules_deploy_key']    = 'NAME_OF_KEY'
